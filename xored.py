b795a487a65c9ce79f99c06bea9a42f5232cea00
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-08-01 19:36:13 +0300

    add xored.py

---
+++ xored.py
@@ -0,0 +1,13 @@
+key = 170
+input_file = "xored"
+output_file = "exe"
+
+with open(input_file, "rb") as f:
+    data = f.read()
+
+decoded = bytes(b ^ key for b in data)
+
+with open(output_file, "wb") as f:
+    f.write(decoded)
+
+print(f"Decryption complete. Output saved to '{output_file}'")
