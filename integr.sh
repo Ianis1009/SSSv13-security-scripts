58696c5331275d575eb1bc9c4b07245e3c00435e
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-08-01 23:24:32 +0300

    update integr.sh

--- integr.sh
+++ integr.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-if ["$#" -ne 1]; then
+if [ "$#" -ne 1 ]; then
     echo "Utilizare: $0 <fisier>"
     exit 1
+fi
 
-fi 
+FILE="$1"
 
-FILE = "$1"
+if [ ! -f "$FILE" ]; then
+    echo "Fisierul nu exista."
+    exit 1
+fi
 
-#TODO
+CHECKSUM=$(sha256sum "$FILE" | awk '{print $1}')
+
+echo "Fisier: $FILE"
+echo "SHA-256: $CHECKSUM"
