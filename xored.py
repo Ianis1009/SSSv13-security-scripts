5d5d306575fd1f90fa1522a853d33fc1af711b4f
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-08-08 11:11:30 +0300

    xored

--- xored.py
+++ xored.py
@@ -1,12 +1,13 @@
+
+# key, input_file, output_file
 key = 170
 input_file = "xored"
-output_file = "exe"
+output_file = "exe" # binary -> ./exe
 
 with open(input_file, "rb") as f:
     data = f.read()
 
 decoded = bytes(b ^ key for b in data)
-
 with open(output_file, "wb") as f:
     f.write(decoded)
 
