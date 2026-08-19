efff19f339e9ec661ebe5dc026e9ce6a2cc9d375
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-08-19 22:25:18 +0300

    update

--- ssh_log.sh
+++ ssh_log.sh
@@ -1,1 +1,8 @@
-# TODO
+#!/usr/bin/env bash
+
+value=$(sshd -T 2>/dev/null | awk '$1 == "permitrootlogin" {print $2}')
+
+[[ "$value" == "no" ]] && echo "OK" || {
+    echo "WARN: PermitRootLogin=$value"
+    exit 1
+}
